e8aa44024f3361f5c9d620063986cedac621620c
ShashyCode <82726693+ShashyCode@users.noreply.github.com>
2021-09-04 02:25:17 +0530

    Update views.py

--- Users/views.py
+++ Users/views.py
@@ -61,7 +61,7 @@ class LoginView(APIView):
         
             payload = {
                 "id": user.id,
-                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=5),
                 "iat" : datetime.datetime.utcnow()
             }
 
